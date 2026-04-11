2d6857207128728f4f2b20be21e7b85382eef84c
ldporsius <linda.d.porsius@gmail.com>
2026-04-11 13:13:07 +0200

    cleanup

--- feature/editing_status/presentation/src/main/java/nl/codingwithlinda/adaptivedesigntokens/feature/editing_status/presentation/NoteScreen.kt
+++ feature/editing_status/presentation/src/main/java/nl/codingwithlinda/adaptivedesigntokens/feature/editing_status/presentation/NoteScreen.kt
@@ -1,7 +1,5 @@
 package nl.codingwithlinda.adaptivedesigntokens.feature.editing_status.presentation
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,8 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
@@ -30,7 +26,6 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -107,7 +102,6 @@ private fun EditingStatusChip(
     modifier: Modifier = Modifier,
 ) {
     if (status == EditingStatus.Idle) return
-
 
     Row(
         modifier = modifier
